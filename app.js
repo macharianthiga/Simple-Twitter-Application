06ae93ee5ff1cefabd1334dbedb086cf29ddc2cb
nthigamac <dmdmasha28@gmail.com>
2017-10-26 16:21:18 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express     = require('express'),
+      morgan      = require('morgan'),
+      bodyParser  = require('body-parser'),
+      mongoose    =  require('mongoose'),
+      hbs         = require('hbs'),
+      expressHbs  = require('express-handlebars'),
+      app         = express();
+
+
+
+
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set(' view engine',  'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3030, (err) => {
+  if (err) console.log(err);
+  console.log('Twitter is running on port 3030');
+});
